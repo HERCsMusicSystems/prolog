24ab1ba2f946068ca01198b31a58b3c5852d1c48
rama <rama@ramas-Mac-mini.local>
2012-11-23 12:42:55 +1030

    OS X build added.

---
+++ osx/operating_system.h
@@ -0,0 +1,12 @@
+///////////////////////////////////////
+// Copyright (C) 2007 Robert P. Wolf //
+//        ALL RIGHTS RESERVED        //
+///////////////////////////////////////
+
+//#define WINDOWS_OPERATING_SYSTEM
+#define LINUX_OPERATING_SYSTEM
+#define MAC_OPERATING_SYSTEM
+
+#define INTERNAL_RESOURCES
+#define MULTICORE_MULTIPROCESSING
+//#define NO_TRACKING
