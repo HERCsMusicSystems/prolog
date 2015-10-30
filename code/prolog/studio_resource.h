df2394fa5f95855d3b88ce71b522b63ee1a716ab
robert <robert@haesemathematics.com.au>
2015-10-30 10:43:55 +1030

    Missing resources for Visual Studio added.

---
+++ code/prolog/studio_resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by studio.rc
+//
+#define IDI_H                           104
+#define IDI_A                           104
+#define STUDIO_PRC                      131
+#define STORE_PRC                       132
+#define HELP_PRC                        133
+#define KEYBOARD_PRC                    134
+#define RECORD_PRC                      135
+#define SCALA_READER_PRC                136
+#define NEURAL_PRC                      137
+#define F1_PRC                          138
+#define CONDUCTOR_PRC                   139
+#define MIDI_PRC                        140
+#define HTTP_PRC                        141
+#define SQL_PRC                         142
+#define TEST_PRC                        143
+#define XML_PRC                         144
+#define DISTRIBUTION_PRC                145
+#define CONTROL_PRC                     146
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        147
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
