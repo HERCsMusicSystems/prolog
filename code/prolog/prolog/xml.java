5b7048671442f272bba66096c3b48d1a5d8981c7
robert <robert@haesemathematics.com.au>
2016-10-10 09:32:08 +1030

    Java source code for processing xml added.

---
+++ code/prolog/prolog/xml.java
@@ -0,0 +1,87 @@
+
+
+package prolog;
+
+import prolog . studio . TermReader;
+import javax . xml . parsers . *;
+import org . w3c . dom . *;
+
+class xml_native_class extends PrologNativeCode {
+	public PrologRoot root;
+	public PrologAtom atomC (String name) {
+		PrologAtom ret = root . search (name);
+		if (ret == null) ret = root . createAtom (name);
+		return ret;
+	}
+	public void drop_node (PrologAtom atom, Node node) {
+		if (node . getNodeType () == Node . TEXT_NODE) {
+			String text = node . getNodeValue ();
+			if (text . trim () . length () < 1) return;
+			root . attachClause (root . pair (root . pair (root . atom (atom), root . pair (root . text (text), root . earth ())), root . earth ())); return;
+		}
+		if (node . getNodeType () != Node . ELEMENT_NODE) return;
+		String node_name = node . getNodeName ();
+		PrologAtom node_atom = new PrologAtom ("<" + node_name + ">");
+		root . attachClause (root . pair (  root . pair (root . atom (atom),
+											root . pair (root . atom (atomC (node_name)),
+											root . pair (root . atom (node_atom),
+											root . earth ()))), root . earth ()));
+		NamedNodeMap attributes = node . getAttributes ();
+		int attribute_count = attributes . getLength ();
+		for (int ind = 0; ind < attribute_count; ind++) {
+			Attr attribute = (Attr) attributes . item (ind);
+			String vv = attribute . getNodeValue ();
+			PrologElement v = null;
+			try {
+				v = root . integer (Integer . parseInt (vv));
+			} catch (Exception ex) {
+				try {
+					v = root . Double (Double . parseDouble (vv));
+				} catch (Exception exx) {
+					v = root . atomC (vv);
+				}
+			}
+			root . attachClause (root . pair (  root . pair (root . atom (node_atom),
+												root . pair (root . atom (atomC (attribute . getNodeName ())),
+												root . pair (v,
+												root . earth ()))), root . earth ()));
+		}
+		Node child = node . getFirstChild ();
+		while (child != null) {drop_node (node_atom, child); child = child . getNextSibling ();}
+	}
+	public boolean code (PrologElement parameters, PrologResolution resolution) {
+		PrologElement path = null;
+		PrologElement atom = null;
+		while (parameters . isPair ()) {
+			PrologElement el = parameters . getLeft ();
+			if (el . isText ()) path = el;
+			if (el . isAtom ()) atom = el;
+			if (el . isVar ()) atom = el;
+			parameters = parameters . getRight ();
+		}
+		if (atom == null || path == null) return false;
+		Document doc = null;
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory . newInstance ();
+			factory . setIgnoringComments (true);
+			doc = factory . newDocumentBuilder () . parse (path . getText ());
+		} catch (Exception ex) {return false;}
+		if (doc == null) return false;
+		Node node = doc . getFirstChild ();
+		if (node == null) return false;
+		if (atom . isVar ()) atom . setAtom (atomC (node . getNodeName ()));
+		drop_node (atom . getAtom (), node);
+		return true;
+	}
+	public xml_native_class (PrologRoot root) {this . root = root;}
+}
+
+public class xml extends PrologServiceClass {
+	public PrologRoot root;
+	public void init (PrologRoot root, PrologDirectory directory) {this . root = root;}
+	public PrologNativeCode getNativeCode (String name) {
+		if (name . equals ("xml")) return new xml_native_class (root);
+		return null;
+	}
+}
+
