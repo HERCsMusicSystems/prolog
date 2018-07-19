f17d103cf082337b8b8e84b5b87bf05d934f319a
rama <rama@hammer.com>
2018-07-19 21:43:29 +0930

    Code objectification and attempt of PrologNativeCode.

--- code/prolog/javascript/prolog.js
+++ code/prolog/javascript/prolog.js
@@ -9,44 +9,64 @@ this . PrologAtom = function (name, root) {
 	this . Protected = false;
 	this . firstClause = null;
 	this . machine = null;
-	this . names = function () {
-		var area = "" + this . name;
-		var sub = this . next;
-		while (sub !== null) {area += " " + sub . name; sub = sub . next;}
-		return area;
-	};
-	this . search = function (name) {
-		var ret = this;
-		while (ret !== null && ret . Privated) {if (ret . name === name) return ret; ret = next;}
-		return null;
-	};
-	this . searchPrivate = function (name) {
-		var ret = this;
-		while (ret !== null) {if (ret . name === name) return ret; ret = next;}
-		return null;
-	};
-	this . setMachine = function (obj) {
-		if (this . firstClause !== null || (this . Protected && obj !== null)) return false;
-		this . Protected = (obj !== null);
-		this . machine = obj;
-		return true;
-	};
-	this . isTypeOfAtom = function (atom) {
-		if (this . machine === null) return false;
-		var machine_type = atom . getMachine ();
-		if (machine_type === null) return false;
-		return this . machine . isTypeOf (machine_type . codeName ());
-	};
-	this . isTypeOf = function (code) {
-		if (this . machine === null) return false;
-		if (code === null) return false;
-		return this . machine . isTypeOf (code . codeName ());
-	};
-	this . machineType = function () {return this . machine === null ? null : this . machine . codeName ();};
-	this . getMachine = function () {return this . machine;};
 };
+this . PrologAtom . prototype . names = function () {
+	var area = "" + this . name;
+	var sub = this . next;
+	while (sub !== null) {area += " " + sub . name; sub = sub . next;}
+	return area;
+};
+this . PrologAtom . prototype . search = function (name) {
+	var ret = this;
+	while (ret !== null && ret . Privated) {if (ret . name === name) return ret; ret = next;}
+	return null;
+};
+this . PrologAtom . prototype . searchPrivate = function (name) {
+	var ret = this;
+	while (ret !== null) {if (ret . name === name) return ret; ret = next;}
+	return null;
+};
+this . PrologAtom . prototype . setMachine = function (obj) {
+	if (this . firstClause !== null || (this . Protected && obj !== null)) return false;
+	this . Protected = (obj !== null);
+	this . machine = obj;
+	return true;
+};
+this . PrologAtom . prototype . isTypeOfAtom = function (atom) {
+	if (this . machine === null) return false;
+	var machine_type = atom . getMachine ();
+	if (machine_type === null) return false;
+	return this . machine . isTypeOf (machine_type . codeName ());
+};
+this . PrologAtom . prototype . isTypeOf = function (code) {
+	if (this . machine === null) return false;
+	if (code === null) return false;
+	return this . machine . isTypeOf (code . codeName ());
+};
+this . PrologAtom . prototype . machineType = function () {return this . machine === null ? null : this . machine . codeName ();};
+this . PrologAtom . prototype . getMachine = function () {return this . machine;};
+
+this . PrologNativeCode = function () {};
+var native_code_name = "PrologNativeCode";
+this . PrologNativeCode . prototype . name = function () {return native_code_name;};
 
 };
 /*
+package prolog;
+
+public class PrologNativeCode {
+	public static String name () {return "PrologNativeCode";}
+	public String codeName () {return name ();}
+	public boolean isTypeOf (String code_name) {return name () == code_name;}
+	public boolean code (PrologElement parameters, PrologResolution resolution) {return true;}
+};
+
+static char * native_code_name = "PrologNativeCode";
+char * PrologNativeCode :: name (void) {return native_code_name;}
+char * PrologNativeCode :: codeName (void) {return name ();}
+bool PrologNativeCode :: code (PrologElement * parameter, PrologResolution * resolution) {return true;}
+bool PrologNativeCode :: isTypeOf (char * code_name) {return name () == code_name;}
+PrologNativeCode :: PrologNativeCode (void) TRACKING (16) {}
+PrologNativeCode :: ~ PrologNativeCode (void) {}
 */
 
