5a76819a05939c86c4a81a93fd304b325d04d4da
Robert Wolf <a1199949@adelaide.edu.au>
2018-07-20 17:00:59 +0930

    PrologDirectory implemented.

--- code/prolog/javascript/prolog.js
+++ code/prolog/javascript/prolog.js
@@ -1,6 +1,10 @@
 
 var prolog = new function () {
 
+var hrcs = this;
+
+//////// ATOM ////////
+
 this . PrologAtom = function (name, root) {
 	if (name === undefined) this . name = "<" + Math . random () . toString (16) . substring (2) + "#" + new Date () . getTime () . toString (16) + ">";
 	else this . name = name + "";
@@ -10,6 +14,7 @@ this . PrologAtom = function (name, root) {
 	this . firstClause = null;
 	this . machine = null;
 };
+/*
 this . PrologAtom . prototype . names = function () {
 	var area = "" + this . name;
 	var sub = this . next;
@@ -26,6 +31,7 @@ this . PrologAtom . prototype . searchPrivate = function (name) {
 	while (ret !== null) {if (ret . name === name) return ret; ret = next;}
 	return null;
 };
+*/
 this . PrologAtom . prototype . setMachine = function (obj) {
 	if (this . firstClause !== null || (this . Protected && obj !== null)) return false;
 	this . Protected = (obj !== null);
@@ -46,10 +52,76 @@ this . PrologAtom . prototype . isTypeOf = function (code) {
 this . PrologAtom . prototype . machineType = function () {return this . machine === null ? null : this . machine . codeName ();};
 this . PrologAtom . prototype . getMachine = function () {return this . machine;};
 
+//////// NATIVE CODE ////////
+
 this . PrologNativeCode = function () {};
 this . PrologNativeCode . prototype . codeName = function () {return "PrologNativeCode";};
 this . PrologNativeCode . prototype . isTypeOf = function (code_name) {return code_name === "PrologNativeCode";};
 this . PrologNativeCode . prototype . code = function () {return true;};
+
+//////// DIRECTORY /////////
+
+this . PrologDirectory = function (name, root, service) {
+	this . name = name + "";
+	this . next = root == null ? null : root;
+	this . firstAtom = null;
+	this . service_class = service == null ? null : service;
+};
+this . PrologDirectory . prototype . getServiceClass = function (name) {
+	if (name === undefined) return this . service_class;
+	if (name === this . name) return this . service_class;
+	if (this . next === null) return null;
+	return this . next . getServiceClass (name);
+};
+this . PrologDirectory . prototype . names = function () {
+	var area = [this . name];
+	var next = this . next;
+	while (next !== null) {area . push (next . name); next = next . next;}
+	return area;
+};
+this . PrologDirectory . prototype . searchAtom = function (search) {
+	if (this . firstAtom === null) return null;
+	var atom = this . firstAtom;
+	while (atom !== null) {if (atom . name === search && ! atom . Privated) return atom; atom = atom . next;}
+	return null;
+};
+this . PrologDirectory . prototype . searchPrivateAtom = function (search) {
+	if (this . firstAtom === null) return null;
+	var atom = this . firstAtom;
+	while (atom !== null) {if (atom . name === search) return atom; atom = atom . next;}
+	return null;
+};
+this . PrologDirectory . prototype . createAtom = function (name) {
+	this . firstAtom = new hrcs . PrologAtom (name, this . firstAtom);
+	return this . firstAtom;
+};
+this . PrologDirectory . prototype . removeAtom = function (atom) {
+	if (this . firstAtom === null) {if (this . next !== null) return this . next . removeAtom (atom); return false;}
+	if (this . firstAtom === atom) {this . firstAtom = this . firstAtom . next; return true;}
+	var ap = this . firstAtom;
+	while (ap . next !== null) {
+		if (ap . next === atom) {ap . next = ap . next . next; return true;}
+		ap = ap . next;
+	}
+	if (this . next === null) return false;
+	return this . next . removeAtom (atom);
+};
+this . PrologDirectory . prototype . list = function () {
+	var area = [];
+	var atom = this . firstAtom;
+	while (atom !== null) {area . push (atom . name); atom = atom . next;}
+	return area;
+};
+this . PrologDirectory . prototype . close = function () {
+	if (this . next === null) return this;
+	var sub = this . next; this . next = this . next . next; sub . next = this;
+	return sub;
+};
+this . PrologDirectory . prototype . duplicate = function (root) {
+	var dir = new hrcs . PrologDirectory (this . name, root, this . service_class);
+	dir . fistAtom = this . firstAtom;
+	return dir;
+};
 
 };
 /*
