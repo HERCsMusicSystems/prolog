de891d49bb031b2b21b119f65557824b48322473
Robert Wolf <robert.wolf@adelaide.edu.au>
2018-07-26 15:06:59 +0930

    Code reduction....

--- code/prolog/javascript/prolog.js
+++ code/prolog/javascript/prolog.js
@@ -5,7 +5,7 @@ var hrcs = this;
 
 //////// ATOM ////////
 
-this . PrologAtom = function (name, root) {
+this . Atom = function (name, root) {
 	if (name === undefined) this . name = "<" + Math . random () . toString (16) . substring (2) + "#" + new Date () . getTime () . toString (16) + ">";
 	else this . name = name + "";
 	this . next = root === undefined ? null : root;
@@ -14,51 +14,51 @@ this . PrologAtom = function (name, root) {
 	this . firstClause = null;
 	this . machine = null;
 };
-this . PrologAtom . prototype . setMachine = function (obj) {
+this . Atom . prototype . setMachine = function (obj) {
 	if (this . firstClause !== null || (this . Protected && obj !== null)) return false;
 	this . Protected = (obj !== null);
 	this . machine = obj;
 	return true;
 };
-this . PrologAtom . prototype . getMachine = function () {return this . machine;};
+this . Atom . prototype . getMachine = function () {return this . machine;};
 
 //////// DIRECTORY /////////
 
-this . PrologDirectory = function (name, root, service) {
+this . Directory = function (name, root, service) {
 	this . name = name + "";
 	this . next = root == null ? null : root;
 	this . firstAtom = null;
 	this . service_class = service == null ? null : service;
 };
-this . PrologDirectory . prototype . getServiceClass = function (name) {
+this . Directory . prototype . getServiceClass = function (name) {
 	if (name === undefined) return this . service_class;
 	if (name === this . name) return this . service_class;
 	if (this . next === null) return null;
 	return this . next . getServiceClass (name);
 };
-this . PrologDirectory . prototype . names = function () {
+this . Directory . prototype . names = function () {
 	var area = [this . name];
 	var next = this . next;
 	while (next !== null) {area . push (next . name); next = next . next;}
 	return area;
 };
-this . PrologDirectory . prototype . searchAtom = function (search) {
+this . Directory . prototype . searchAtom = function (search) {
 	if (this . firstAtom === null) return null;
 	var atom = this . firstAtom;
 	while (atom !== null) {if (atom . name === search && ! atom . Privated) return atom; atom = atom . next;}
 	return null;
 };
-this . PrologDirectory . prototype . searchPrivateAtom = function (search) {
+this . Directory . prototype . searchPrivateAtom = function (search) {
 	if (this . firstAtom === null) return null;
 	var atom = this . firstAtom;
 	while (atom !== null) {if (atom . name === search) return atom; atom = atom . next;}
 	return null;
 };
-this . PrologDirectory . prototype . createAtom = function (name) {
-	this . firstAtom = new hrcs . PrologAtom (name, this . firstAtom);
+this . Directory . prototype . createAtom = function (name) {
+	this . firstAtom = new hrcs . Atom (name, this . firstAtom);
 	return this . firstAtom;
 };
-this . PrologDirectory . prototype . removeAtom = function (atom) {
+this . Directory . prototype . removeAtom = function (atom) {
 	if (this . firstAtom === null) {if (this . next !== null) return this . next . removeAtom (atom); return false;}
 	if (this . firstAtom === atom) {this . firstAtom = this . firstAtom . next; return true;}
 	var ap = this . firstAtom;
@@ -69,19 +69,19 @@ this . PrologDirectory . prototype . removeAtom = function (atom) {
 	if (this . next === null) return false;
 	return this . next . removeAtom (atom);
 };
-this . PrologDirectory . prototype . list = function () {
+this . Directory . prototype . list = function () {
 	var area = [];
 	var atom = this . firstAtom;
 	while (atom !== null) {area . push (atom . name); atom = atom . next;}
 	return area;
 };
-this . PrologDirectory . prototype . close = function () {
+this . Directory . prototype . close = function () {
 	if (this . next === null) return this;
 	var sub = this . next; this . next = this . next . next; sub . next = this;
 	return sub;
 };
-this . PrologDirectory . prototype . duplicate = function (root) {
-	var dir = new hrcs . PrologDirectory (this . name, root, this . service_class);
+this . Directory . prototype . duplicate = function (root) {
+	var dir = new hrcs . Directory (this . name, root, this . service_class);
 	dir . fistAtom = this . firstAtom;
 	return dir;
 };
@@ -89,18 +89,18 @@ this . PrologDirectory . prototype . duplicate = function (root) {
 //////// ELEMENET ////////
 // 0: EARTH, 1: PAIR, 2: VAR, 3: ATOM, 4: SLASH, 5: FAIL, 6: NATIVE
 
-this . PrologElement = function () {this . type = 0; return;};
+this . Element = function () {this . type = 0; return;};
 
-this . PrologElement . prototype . setPair = function () {
+this . Element . prototype . setPair = function () {
 	this . type = 1;
-	this . left = new hrcs . PrologElement ();
-	this . right = new hrcs . PrologElement ();
+	this . left = new hrcs . Element ();
+	this . right = new hrcs . Element ();
 };
-this . PrologElement . prototype . setVar = function (ind) {this . type = 2; this . left = ind;};
-this . PrologElement . prototype . setAtom = function (atom) {this . type = 3; this . left = atom;};
-this . PrologElement . prototype . setNative = function (el) {this . type = 6; this . left = el;};
-this . PrologElement . prototype . duplicate = function (el) {
-	if (el === undefined) el = new hrcs . PrologElement ();
+this . Element . prototype . setVar = function (ind) {this . type = 2; this . left = ind;};
+this . Element . prototype . setAtom = function (atom) {this . type = 3; this . left = atom;};
+this . Element . prototype . setNative = function (el) {this . type = 6; this . left = el;};
+this . Element . prototype . duplicate = function (el) {
+	if (el === undefined) el = new hrcs . Element ();
 	el . type = this . type;
 	switch (this . type) {
 		case 0: case 4: case 5: return el;
@@ -112,7 +112,7 @@ this . PrologElement . prototype . duplicate = function (el) {
 	}
 	return el;
 };
-this . PrologElement . prototype . attach = function (position) {
+this . Element . prototype . attach = function (position) {
 	if (this . type !== 1) return false;
 	var sub = this . left; if (sub . type !== 1) return false;
 	sub = sub . left; if (sub . type !== 3) return false;
@@ -128,7 +128,7 @@ this . PrologElement . prototype . attach = function (position) {
 
 //////// ROOT ////////
 
-this . PrologRoot = function () {
+this . Root = function () {
 	this . left_caption = '[';
 	this . right_caption = ']';
 	this . var_caption = '*';
@@ -138,9 +138,9 @@ this . PrologRoot = function () {
 	this . fail_caption = 'fail';
 	this . quotation_caption = '"';
 	this . auto_atoms = false;
-	this . root = new hrcs . PrologDirectory ("user!");
+	this . root = new hrcs . Directory ("user!");
 };
-this . PrologRoot . prototype . getValue = function (el) {
+this . Root . prototype . getValue = function (el) {
 	if (el == null) return "<null element>";
 	switch (el . type) {
 		case 0: return this . left_caption + this . right_caption;
@@ -156,7 +156,7 @@ this . PrologRoot . prototype . getValue = function (el) {
 	}
 	return "Unknown element type.";
 };
-this . PrologRoot . prototype . getRightCaption = function (el) {
+this . Root . prototype . getRightCaption = function (el) {
 	if (el == null) return "<null element>";
 	switch (el . type) {
 		case 0: return '';
@@ -166,17 +166,17 @@ this . PrologRoot . prototype . getRightCaption = function (el) {
 	}
 	return ' ' + this . mid_caption + ' ' + this . getValue (el);
 };
-this . PrologRoot . prototype . getServiceClass = function (name) {
+this . Root . prototype . getServiceClass = function (name) {
 	if (this . root === null) return null;
 	return this . root . getServiceClass (name);
 };
-this . PrologRoot . prototype . createDirectory = function (name, service) {this . root = new hrcs . PrologDirectory (name, this . root, service);};
-this . PrologRoot . prototype . searchDirectory = function (name) {
+this . Root . prototype . createDirectory = function (name, service) {this . root = new hrcs . Directory (name, this . root, service);};
+this . Root . prototype . searchDirectory = function (name) {
 	var ret = this . root;
 	while (ret !== null) {if (ret . name === name) return ret; ret = ret . next;}
 	return null;
 };
-this . PrologRoot . prototype . search = function (name) {
+this . Root . prototype . search = function (name) {
 	var sub = this . root;
 	while (sub !== null) {
 		var ret = sub . searchAtom (name);
@@ -185,7 +185,7 @@ this . PrologRoot . prototype . search = function (name) {
 	}
 	return null;
 };
-this . PrologRoot . prototype . list = function (name) {
+this . Root . prototype . list = function (name) {
 	if (name === undefined) {
 		if (this . root === null) return [];
 		return this . root . names ();
@@ -194,8 +194,8 @@ this . PrologRoot . prototype . list = function (name) {
 	if (sub === null) return [];
 	return sub . list ();
 };
-this . PrologRoot . prototype . close = function () {if (this . root === null) return; this . root . close ();};
-this . PrologRoot . prototype . drop = function (name) {
+this . Root . prototype . close = function () {if (this . root === null) return; this . root . close ();};
+this . Root . prototype . drop = function (name) {
 	if (this . root === null) return;
 	if (name === undefined) {this . root = this . root . next; return;}
 	if (this . root . name === name) this . root = this . root . next;
@@ -204,32 +204,32 @@ this . PrologRoot . prototype . drop = function (name) {
 		if (sub . next . name === name) {sub . next = sub . next . next; return;}
 	}
 };
-this . PrologRoot . prototype . createAtom = function (name) {
+this . Root . prototype . createAtom = function (name) {
 	if (this . root === null) return null;
 	return this . root . createAtom (name);
 };
-this . PrologRoot . prototype . removeAtom = function (name) {
+this . Root . prototype . removeAtom = function (name) {
 	if (this . root === null) return false;
 	return this . root . removeAtom (name);
 };
-this . PrologRoot . prototype . searchC = function (name) {
+this . Root . prototype . searchC = function (name) {
 	var atom = this . search (name);
 	if (atom === null) return this . createAtom (name);
 	return atom;
 };
-this . PrologRoot . prototype . Private = function (name) {
+this . Root . prototype . Private = function (name) {
 	if (this . root === null) return false;
 	var atom = this . root . searchPrivateAtom (name);
 	if (atom === null) return false;
 	atom . Privated = true;
 };
-this . PrologRoot . prototype . Protect = function (name) {
+this . Root . prototype . Protect = function (name) {
 	if (this . root === null) return false;
 	var atom = this . root . searchPrivateAtom (name);
 	if (atom === null) return false;
 	atom . Protected = true;
 };
-this . PrologRoot . prototype . listAtom = function (name) {
+this . Root . prototype . listAtom = function (name) {
 	var atom = this . search (name);
 	if (atom === null) return [];
 	var el = atom . firstClause;
@@ -241,16 +241,15 @@ this . PrologRoot . prototype . listAtom = function (name) {
 	return ret;
 };
 
-//////// ROOT ////////
+//////// READER ////////
 
-this . PrologReader = function (root, file) {
+this . Reader = function (root, file) {
 	this . root = root;
 	this . file = file;
 	this . act = '';
 	this . ind = 0;
 };
-
-this . PrologReader . prototype . move = function () {
+this . Reader . prototype . move = function () {
 	this . act = this . file . charAt (this . ind ++);
 	return this . act;
 };
@@ -258,46 +257,46 @@ this . PrologReader . prototype . move = function () {
 };
 /*
 */
-root = new prolog.PrologRoot();
+root = new prolog.Root();
 sonda = root.createAtom('sonda');
 mariner = root.createAtom('mariner');
 //////////
-e = new prolog.PrologElement();
+e = new prolog.Element();
 e.setPair();
 e.left.setAtom(sonda);
 e.right.setPair();
 e.right.left.setAtom(mariner);
-cl = new prolog.PrologElement();
+cl = new prolog.Element();
 cl.setPair();
 cl.left = e;
 console . log (cl.attach());
 ///////
-e = new prolog.PrologElement();
+e = new prolog.Element();
 e.setPair();
 e.left.setAtom(sonda);
 e.right.setPair();
 e.right.left.setNative(123);
-cl = new prolog.PrologElement();
+cl = new prolog.Element();
 cl.setPair();
 cl.left = e;
 console . log (cl.attach());
 ///////
-e = new prolog.PrologElement();
+e = new prolog.Element();
 e.setPair();
 e.left.setAtom(sonda);
 e.right.setPair();
 e.right.left.setNative(124);
-cl = new prolog.PrologElement();
+cl = new prolog.Element();
 cl.setPair();
 cl.left = e;
 console . log (cl.attach());
 ///////
-e = new prolog.PrologElement();
+e = new prolog.Element();
 e.setPair();
 e.left.setAtom(sonda);
 e.right.setPair();
 e.right.left.setVar(124);
-cl = new prolog.PrologElement();
+cl = new prolog.Element();
 cl.setPair();
 cl.left = e;
 console . log (cl.attach(0));
