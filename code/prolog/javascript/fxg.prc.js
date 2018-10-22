8d8dc2600df00dd9a6c8f18c779c6bcf3a28a53c
Robert Wolf <a1199949@adelaide.edu.au>
2018-10-23 10:19:44 +1030

    Viewport now resizeable....

--- code/prolog/javascript/fxg.prc.js
+++ code/prolog/javascript/fxg.prc.js
@@ -2,12 +2,12 @@
 studio . setResource (['prolog', 'fxg'],
 function (root, directory) {
   var viewport = function (atom) {
-    var bar = document . createElement ('div'); bar . innerHTML = 'bar';
-    //var content = document . createElement ('div'); content . innerHTML = 'VOYAGER<br />SPUTNIK<br />SONDA';
-    var content = document . createElement ('canvas');
-    content . id = 'cx';
-    var info = document . createElement ('div');
-    var resize = document . createElement ('input'); resize . type = 'button'; resize . value = 'x'; console . log (resize);
+    var bar = document . createElement ('div'); bar . style . background = 'yellow'; bar . appendChild (document . createTextNode ('sonda'));
+    var close = document . createElement ('input'); close . type = 'button'; close . value = String . fromCharCode (0xd7); close . style . float = 'right';
+    bar . appendChild (close);
+    var content = document . createElement ('canvas'); content . id = 'cx';
+    var info = document . createElement ('div'); info . style . background = 'yellow'; info . appendChild (document . createTextNode ('info'));
+    var resize = document . createElement ('input'); resize . type = 'button'; resize . value = String . fromCharCode (0x21f2); resize . style . float = 'right';
     info . appendChild (resize);
     var div = document . createElement ('div');
     var divp = {x: 200, y: 100};
@@ -15,12 +15,13 @@ function (root, directory) {
     div . appendChild (content);
     div . appendChild (info);
     div . style . background = 'blue'; div . style . position = 'absolute'; div . style . top = divp . y; div . style . left = divp . x;
-    info . align = 'right';
     var mouseup = function (e) {document . onmouseup = null; document . onmousemove = null;};
     var mousemove = function (e) {divp . x += e . movementX; divp . y += e . movementY; div . style . top = divp . y; div . style . left = divp . x;};
+    var mousesize = function (e) {content . width += e . movementX; content . height += e . movementY;};
     bar . onmousedown = function (e) {document . onmouseup = mouseup; document . onmousemove = mousemove;};
-    resize = onmousedown = function (e) {document . onmouseup = mouseup; document . onmousemove = mousemove;};
+    resize . onmousedown = function (e) {document . onmouseup = mouseup; document . onmousemove = mousesize;};
     document . body . appendChild (div);
+    close . onmousedown = function (e) {div . parentElement . removeChild (div); atom . setMachine (null);};
     this . code = function (el) {
       if (el . type === 0) {div . parentElement . removeChild (div); return atom . setMachine (null);}
       return true;
