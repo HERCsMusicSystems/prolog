23b59e0562df2789cc18f569758f41176442d496
Robert Wolf <a1199949@adelaide.edu.au>
2018-10-23 12:56:00 +1030

    Parametrised viewport and work-in-progress on making viewport responsive.

--- code/prolog/javascript/fxg.prc.js
+++ code/prolog/javascript/fxg.prc.js
@@ -1,16 +1,23 @@
 
 studio . setResource (['prolog', 'fxg'],
 function (root, directory) {
-  var viewport = function (atom) {
-    var bar = document . createElement ('div'); bar . style . background = 'yellow'; bar . appendChild (document . createTextNode ('sonda'));
+  var Location = directory . searchAtom ('Location');
+  var Position = directory . searchAtom ('Position');
+  var Size = directory . searchAtom ('Size');
+  var Scaling = directory . searchAtom ('Scaling');
+  var Mode = directory . searchAtom ('Mode');
+  var viewport = function (atom, name, x, y, width, height) {
+    var bar = document . createElement ('div'); bar . style . background = 'yellow'; bar . appendChild (document . createTextNode (name === null ? 'VIEWPORT' : name)); bar . style ['font-family'] = 'arial';
     var close = document . createElement ('input'); close . type = 'button'; close . value = String . fromCharCode (0xd7); close . style . float = 'right';
     bar . appendChild (close);
     var content = document . createElement ('canvas'); content . id = 'cx';
-    var info = document . createElement ('div'); info . style . background = 'yellow'; info . appendChild (document . createTextNode ('info'));
+    if (width !== null) content . width = width;
+    if (height !== null) content . height = height;
+    var info = document . createElement ('div'); info . style . background = 'yellow'; info . appendChild (document . createTextNode ('info')); info . style ['font-family'] = 'arial';
     var resize = document . createElement ('input'); resize . type = 'button'; resize . value = String . fromCharCode (0x21f2); resize . style . float = 'right';
     info . appendChild (resize);
     var div = document . createElement ('div');
-    var divp = {x: 200, y: 100};
+    var divp = {x: x === null ? 0 : x, y: y === null ? 0 : y};
     div . appendChild (bar);
     div . appendChild (content);
     div . appendChild (info);
@@ -24,21 +31,36 @@ function (root, directory) {
     close . onmousedown = function (e) {div . parentElement . removeChild (div); atom . setMachine (null);};
     this . code = function (el) {
       if (el . type === 0) {div . parentElement . removeChild (div); return atom . setMachine (null);}
+      if (el . type !== 1) return false;
+      var selector = el . left;
+      if (selector . type === 3) {
+        console . log (selector . left, Location);
+        if (selector . left === Location) {return true;}
+        return false;
+      }
       return true;
     };
   };
   var Viewport = {
     code: function (el) {
-      var atom = null;
+      var atom = null, name = null, x = null, y = null, width = null, height = null;
       while (el . type === 1) {
         var e = el . left;
         if (e . type === 2 || e . type === 3) atom = e;
+        if (e . type === 6) {
+          if (typeof (e . left) === 'number') {
+            if (x === null) x = e . left;
+            else if (y === null) y = e . left;
+            else if (width === null) width = e . left;
+            else height = e . left;
+          } else name = e . left;
+        }
         el = el . right;
       }
       if (atom === null) return false;
       if (atom . type === 2) atom . setAtom (new prolog . Atom ());
       if (atom . left . machine !== null) return false;
-      return atom . left . setMachine (new viewport (atom . left));
+      return atom . left . setMachine (new viewport (atom . left, name, x, y, width, height));
     }
   };
   this . getNativeCode = function (name) {
@@ -55,7 +77,10 @@ function (root, directory) {
 
 studio . setResource (['fxg.prc'], `
 program fxg #machine := 'prolog . fxg'
-  [Viewport]
+  [
+    Viewport
+    Location Position Size Scaling Mode
+  ]
 
 #machine Viewport := 'Viewport'
 
